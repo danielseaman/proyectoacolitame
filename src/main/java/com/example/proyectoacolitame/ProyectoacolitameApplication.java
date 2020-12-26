8929e0c7dcbfce91859621002931c92703c96590
Daniel Seaman <daniel.seaman@ucuenca.edu.ec>
2020-12-26 10:50:14 -0500

    Initial commit

---
+++ src/main/java/com/example/proyectoacolitame/ProyectoacolitameApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyectoacolitame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoacolitameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoacolitameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/proyectoacolitame/ProyectoacolitameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyectoacolitame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoacolitameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
